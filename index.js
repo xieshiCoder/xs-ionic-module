b37494fd2cc0696094c05b2def9987dc037793f6
xieshiCoder <13233076263@163.com>
2017-05-22 09:47:59 +0800

    README.md更新

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /**
  * Created by xieshi on 2017/5/19.
  */
+require('./src/common.css');
 var xsCityPicker = require('./src/citypicker/citypicker');
 var xsImgSlide = require('./src/imgFullScreenSlide/imgFullScreenSlide');
 var xsImgLazyLoad = require('./src/imgLazyLoad/imgLazyLoad');
