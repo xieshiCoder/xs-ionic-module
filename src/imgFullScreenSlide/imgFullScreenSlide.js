4d0430c3eed000393d2ce638c6f91bfcefbd9847
xieshiCoder <13233076263@163.com>
2017-05-23 18:32:08 +0800

    图片全屏预览组件更新

--- src/imgFullScreenSlide/imgFullScreenSlide.js
+++ src/imgFullScreenSlide/imgFullScreenSlide.js
@@ -25,9 +25,50 @@ function xsImgSlide($ionicModal,$ionicSlideBoxDelegate,$timeout){
                     return;
                 }else {
                     scope.modal.show();
+                    $timeout(()=>{
+                        //恢复
+                        angular.element(document.querySelectorAll('.imgs-container')).css('transform',"translate3d(0,0,0)");
+                        angular.element(document.querySelectorAll('.slidebg')).css('background','rgba(0,0,0,1)');
+                    },100);
                 }
                 vm.navNum = (vm.currentIndex+1)+'/'+vm.imgArr.length;
                 $ionicSlideBoxDelegate.$getByHandle('imgslide').slide(vm.currentIndex);
+            }
+            vm.touch = function(){
+                event.preventDefault();
+                let touch = event.gesture.touches[0];
+                let target = touch.target;
+                !target.style.transform && (target.style.transform = "translate3d(0,0,0)");
+                vm.start = {
+                    x : touch.pageX,
+                    y : touch.pageY
+                }
+                vm.bg = 1;
+            }
+            vm.drag = function(){
+                event.preventDefault();
+                let touch = event.gesture.touches[0];
+                let offsetHeight = touch.target.offsetHeight;
+                let sy = touch.pageY - vm.start.y;
+                this.bg-=0.03;
+                sy>0 && (
+                    angular.element(document.querySelectorAll('.imgs-container')).css('transform',"translate3d(0,"+sy+"px,0)"),
+                    angular.element(document.querySelectorAll('.slidebg')).css('background','rgba(0,0,0,'+this.bg+')')
+                );
+            }
+            vm.release = function(){
+                event.preventDefault();
+                let touch = event.gesture.touches[0];
+                let offsetHeight = touch.target.offsetHeight;
+                let re = document.querySelector('.imgs-container').style.transform.match(/\((.+),(.+),(.+)\)/);
+                let nowY = Number(re[2].match(/-?[0-9]*[0-9]*[0-9]/)[0]);
+                if(nowY>=(offsetHeight/3)){
+                    angular.element(document.querySelectorAll('.slidebg')).css('background','rgba(0,0,0,0)');
+                    vm.closeModal();
+                }else{
+                    angular.element(document.querySelectorAll('.imgs-container')).css('transform',"translate3d(0,0,0)");
+                    angular.element(document.querySelectorAll('.slidebg')).css('background','rgba(0,0,0,1)');
+                }
             }
             vm.closeModal = function(){
                 $timeout(function() {
